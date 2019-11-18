25192b937f342b9b37236f0a642fe56bdce89f40
Richard Marko <srk@48.io>
2019-11-18 11:43:26 +0100

    actually honor pinned nixpkgs.nix

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
-{ nixpkgs ? import ./nixpkgs.nix, compiler ? "ghc881" }:
+{ nixpkgs ? import ./nixpkgs.nix {}, compiler ? "ghc881" }:
 let
   overlays = import ./overlay.nix compiler;
-  pkgs = import <nixpkgs> { inherit overlays; };
+  pkgs = import ./nixpkgs.nix { inherit overlays; };
 in
 #pkgs.myHaskellPackages.ivory-bsp-stm32
 pkgs.myHaskellPackages.ivory-tower-helloworld
--- nixpkgs.nix
+++ nixpkgs.nix
@@ -3,14 +3,11 @@
 #
 let
   rev="86ccc63008204a8ab5a7a52645bfb4ade3857818";
-  hash="0fyd9spg0szyh7d445pah8l7jq8s1v6dybqfcfswz99j3iq4kfnl";
+  hash="133vkglgnrrk0jqms2iky5f1fli7bbk2idaxli2wk66isc66lbnm";
 in
 import (
   fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
     sha256 = hash;
   }
-) {
-  config = {};
-  overlays = [];
-}
+)
