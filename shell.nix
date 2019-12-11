1963b336c31b77dcd6f0292a0f493b7d1b45deb0
Richard Marko <srk@48.io>
2019-12-11 10:57:13 +0100

    shell: add gnumake and gcc-arm-embedded to buildInputs

--- shell.nix
+++ shell.nix
@@ -1,2 +1,17 @@
-{ nixpkgs ? import ./nixpkgs.nix, compiler ? "ghc881" }:
-(import ./default.nix { inherit nixpkgs compiler; }).env
+{ nixpkgs ? import ./nixpkgs.nix {}, compiler ? "ghc881" }:
+let
+  default = (import ./default.nix { inherit nixpkgs compiler; });
+in
+nixpkgs.stdenv.mkDerivation {
+  name = "ivory-tower-shell";
+
+  buildInputs = [
+    default.env.nativeBuildInputs
+    nixpkgs.gnumake
+    nixpkgs.gcc-arm-embedded
+  ];
+
+  shellHook = ''
+    echo "Ivory Tower shell"
+  '';
+}
