9b54a810fd47c84968de7e72e9f9ab7cd32fa259
sorki <srk@48.io>
2026-04-07 13:48:00 +0200

    drop stray nix/hastache.nix

--- nix/hastache.nix
+++
@@ -1,9 +0,0 @@
-self: super:
-{
- myHaskellPackages = super.myHaskellPackages.override (old: {
-    overrides = super.lib.composeExtensions (old.overrides or (_: _: {}))
-      (hself: hsuper: {
-        hastache = hself.callPackage ./packages/hastache.nix {};
-      });
-    });
-}
