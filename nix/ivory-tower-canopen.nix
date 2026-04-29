9e43501b31caf468dc910a5b112f3a2e07b14261
sorki <srk@48.io>
2026-04-29 15:31:21 +0200

    ivory-tower-canopen: neaten, dontHaddock generated schemas

--- nix/ivory-tower-canopen.nix
+++ nix/ivory-tower-canopen.nix
@@ -17,13 +17,21 @@ in
  myHaskellPackages = super.myHaskellPackages.override (old: {
     overrides = super.lib.composeExtensions (old.overrides or (_: _: {}))
       (hself: hsuper: {
-        ivory-tower-canopen = super.haskell.lib.dontHaddock ( hself.callCabal2nix "ivory-tower-canopen" "${src}" {} );
-        ivory-tower-canopen-core = super.haskell.lib.dontHaddock ( hself.callCabal2nix "ivory-tower-canopen-core" "${src}/ivory-tower-canopen-core" {} );
+        ivory-tower-canopen = super.haskell.lib.dontHaddock (
+          hself.callCabal2nix "ivory-tower-canopen" "${src}" {}
+        );
+        ivory-tower-canopen-core = super.haskell.lib.dontHaddock (
+          hself.callCabal2nix "ivory-tower-canopen-core" "${src}/ivory-tower-canopen-core" {}
+        );
         ivory-tower-canopen-src = src;
         ivory-tower-canopen-schema = hself.mkSchema
           "canopen" "${src}/schema" hself.cidl;
-        canopen-schema-tower = hself.callCabal2nix "canopen-schema-tower" "${hself.ivory-tower-canopen-schema}/canopen-schema-tower" {};
-        canopen-schema-native = hself.callCabal2nix "canopen-schema-native" "${hself.ivory-tower-canopen-schema}/canopen-schema-native" {};
+        canopen-schema-tower = super.haskell.lib.dontHaddock (
+          hself.callCabal2nix "canopen-schema-tower" "${hself.ivory-tower-canopen-schema}/canopen-schema-tower" {}
+        );
+        canopen-schema-native =  super.haskell.lib.dontHaddock (
+          hself.callCabal2nix "canopen-schema-native" "${hself.ivory-tower-canopen-schema}/canopen-schema-native" {}
+        );
       });
   });
 }
